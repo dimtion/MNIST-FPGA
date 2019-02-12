f20f5e71ccca34ce750540746a8b7fb2f43d1137
Loic Carr <loic.carr@imt-atlantique.net>
2019-02-12 11:43:32 +0100

    Top upto subneurone1

--- VHDL/FCNN_top_unit.vhd
+++ VHDL/FCNN_top_unit.vhd
@@ -9,7 +9,7 @@ entity FCNN_top_unit is
 			  G_NBITS_PIXEL     : NATURAL := 8;   -- Number of bits for the input pixels.
 			  G_NUMBER_PIXELS   : NATURAL := 784; -- Number of input pixels in an image.
 			  G_NUMBER_CLASSES  : NATURAL := 10;  -- Number of classes (= Depth of the output layer)
-              G_NBITS_WEIGHTS   : NATURAL := 8;   -- Number of bits for the weights.
+        G_NBITS_WEIGHTS   : NATURAL := 8;   -- Number of bits for the weights.
 			  G_LUT_FOLDER      : STRING  := "../LUT_files"   -- Path of the folder containing the LUTs.
 	 );
     Port ( 
@@ -69,8 +69,8 @@ architecture Behavioral of FCNN_top_unit is
 
     Component CreateWord is
 	    port (
-            I_clk       : in std_logic;
-            I_rst       : in std_logic;
+        I_clk       : in std_logic;
+        I_rst       : in std_logic;
 	    	I_pixel 	: in std_logic_vector(7 downto 0);
 		    I_en_load 	: in std_logic;
 		    I_en_C_P	: in std_logic;
@@ -96,7 +96,7 @@ architecture Behavioral of FCNN_top_unit is
     component Counter_L1 is 
         generic (
 		    N_size : NATURAL := 5;
-		    W_size : NATURAL := 5
+		    W_size : NATURAL := 7
 	    );
 	    port (
 		    I_clk 		: in std_logic;
@@ -116,7 +116,7 @@ architecture Behavioral of FCNN_top_unit is
 		    I_W 	: in std_logic_vector(28*5 -1 downto 0);
 		    I_C 	: in std_logic_vector(6 downto 0);
 		    I_biais	: in std_logic_vector(4 downto 0);
-            O_d     : out std_logic_vector(7 downto 0)
+        O_d     : out std_logic_vector(7 downto 0)
         );
     end component;
 
@@ -256,9 +256,9 @@ architecture Behavioral of FCNN_top_unit is
 
     component DualPort_RAM is
         generic (
-            G_DEPTH      : NATURAL 
-            G_WordLength : NATURAL  
-            G_STYLE      : STRING   
+            G_DEPTH      : NATURAL;
+            G_WordLength : NATURAL;
+            G_STYLE      : STRING
         );
         port (
        
@@ -314,64 +314,115 @@ signal O_arg : std_logic;
 
 signal I_O : std_logic_vector(223 downto 0);
 signal en_I_O : std_logic;
-
 signal I_1 : std_logic_vector(223 downto 0);
 
+signal pixel_count : std_logic_vector(5 downto 0);
+
+-- RAM outputs
+signal img_l1 : std_logic_vector(224-1 downto 0);
+signal O_W_1 : std_logic_vector(7-1 downto 0);
+signal W_1 : std_logic_vector(140-1 downto 0);
+signal B_1 : std_logic_vector(5-1 downto 0);
+signal O_N_1 : std_logic_vector(5-1 downto 0);
+
+
+-- SubNeuron1 outputs
+signal O_Subneurone : std_logic_vector(7 downto 0);
 
 begin
 
     Fsm_top : FSM
         port map(    
-            i_clk   => I_clk,
-		    I_rst   => I_aync_rst,
-		    i_ack   => I_ackPixel,
-		    i_w_0   => I_W_0,
-		    i_p_0   => I_P_0,
-		    i_n_1   => I_N_1,
-		    i_w_1   => I_W_1,
-		    i_n_2   => I_N_2,
-		    i_w_2   => I_W_2,
-		    i_n_3   => I_N_3,
-		    i_arg   => I_arg,
-		    o_request => O_requestPixel,		
-		    o_en_load => en_load,		
-		    o_en_c_w => en_C_W,		
-		    o_en_c_p => en_C_P,
-		    o_w_1_en => W_1_en,		
-		    o_n_1_en => N_1_en,
-		    o_w_2_en => W_2_en,
-		    o_n_2_en => N_2_en,
-		    o_n_3_en => N_3_en,
-		    o_classifvalid => O_classifValid,
-		    o_arg => O_arg
+		i_clk   => I_clk,
+		I_rst   => I_aync_rst,
+		i_ack   => I_ackPixel,
+		i_w_0   => I_W_0,
+		i_p_0   => I_P_0,
+		i_n_1   => I_N_1,
+		i_w_1   => I_W_1,
+		i_n_2   => I_N_2,
+		i_w_2   => I_W_2,
+		i_n_3   => I_N_3,
+		i_arg   => I_arg,
+		o_request => O_requestPixel,		
+		o_en_load => en_load,		
+		o_en_c_w => en_C_W,		
+		o_en_c_p => en_C_P,
+		o_w_1_en => W_1_en,		
+		o_n_1_en => N_1_en,
+		o_w_2_en => W_2_en,
+		o_n_2_en => N_2_en,
+		o_n_3_en => N_3_en,
+		o_classifvalid => O_classifValid,
+		o_arg => O_arg
 		);
     
     CreateWord_1 : CreateWord 
 	port map(
-            I_clk       => I_clk,
-            I_rst       => I_aync_rst,
-	    	I_pixel     => I_pixel,	
-		    I_en_load   => en_load,
-		    I_en_C_P	=> en_C_P,
-		    I_en_C_W 	=> en_C_W,
-		    O_I_0		=> I_O,
-		    O_en_I_0 	=> en_I_O
+		I_clk       => I_clk,
+		I_rst       => I_aync_rst,
+		I_pixel     => I_pixel,	
+		I_en_load   => en_load,
+		I_en_C_P	=> en_C_P,
+		I_en_C_W 	=> en_C_W,
+		O_I_0		=> I_O,
+		O_en_I_0 	=> en_I_O
 	    );
+		Ram_W_1_1 : Ram_W_1
+	generic map (
+			size_w  => 140,
+			addr_size => 1120
+		)
+	port map (
+			I_clk => I_clk,
+			I_rst => I_aync_rst,
+			addr_r => O_W_1,
+			data_r => W_1	
+		);
+	Ram_B_1_1 : Ram_B_1
+	generic map (
+			size_w  => 5,
+			addr_size => 200
+		)
+	port map (
+			I_clk => I_clk,
+			I_rst => I_aync_rst,
+			addr_r => O_W_1,
+			data_r => B_1	
+		);
+	Counter_L1_1 : Counter_L1
+	port map (
+			I_clk => I_clk,
+			I_rst => I_aync_rst,
+			I_N_1_en => W_1_en,
+			I_W_1_en => N_1_en,
+			O_N_1 => O_N_1,
+			O_W_1 => O_W_1
+	);
+	SubNeurone_L1_1 : SubNeurone_l1
+	port map (
+		I_clk => I_clk,
+		I_rst => I_aync_rst,
+		I_data => img_l1,
+		I_W => W_1,
+		I_C => O_W_1,
+		I_biais => B_1,
+		O_d => O_Subneurone
+	);
+  Ram_I : DualPort_RAM 
+  generic map(
+    G_DEPTH         => 28,
+    G_WordLength    => 224,
+    G_STYLE         => "distributed"
+  )
+  port map(
+    I_clk => I_clk,
+    I_write => en_I_O,
+    I_addr_write =>  unsigned(O_W_1), -- word counter L1
+    I_dataWrite => I_O,
+    I_addr_read => unsigned(pixel_count),  -- pixel counter
+    O_dataRead => img_l1
+	);
 
-
-       
-    Ram_I : DualPort_RAM 
-        generic map(
-            G_DEPTH         => 28,
-            G_WordLength    => 224,
-            G_STYLE         => "distributed"
-        );
-        port map(
-            I_clk => I_clk,
-            I_write => 
-            I_addr_wite => 
-            I_dataWrite => 
-            I_addr_read => 
-            O_dataRead => 
 end Behavioral;
 
